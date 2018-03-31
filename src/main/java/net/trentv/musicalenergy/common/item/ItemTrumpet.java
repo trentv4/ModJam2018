6027c586a1edddf46bef646d61d235e15deace83
trentv4 <trentvanslyke@gmail.com>
2018-03-31 11:25:45 -0400

    Add item models for non-dooting and dooting trumpets

--- src/main/java/net/trentv/musicalenergy/common/item/ItemTrumpet.java
+++ src/main/java/net/trentv/musicalenergy/common/item/ItemTrumpet.java
@@ -1,16 +1,22 @@
 package net.trentv.musicalenergy.common.item;
 
+import javax.annotation.Nullable;
+
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumAction;
+import net.minecraft.item.IItemPropertyGetter;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import net.trentv.musicalenergy.common.MusicalObjects;
 
 public class ItemTrumpet extends Item
@@ -18,6 +24,15 @@ public class ItemTrumpet extends Item
 	public ItemTrumpet()
 	{
 		setUnlocalizedName("trumpet");
+		this.addPropertyOverride(new ResourceLocation("blocking"), new IItemPropertyGetter()
+		{
+			@Override
+			@SideOnly(Side.CLIENT)
+			public float apply(ItemStack stack, @Nullable World worldIn, @Nullable EntityLivingBase entityIn)
+			{
+				return entityIn != null && entityIn.isHandActive() && entityIn.getActiveItemStack() == stack ? 1.0F : 0.0F;
+			}
+		});
 	}
 
 	@Override
