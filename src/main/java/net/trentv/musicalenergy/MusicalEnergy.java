77d82e7c11e69bcca0ed93ab28a59379e49580f8
trentv4 <trentvanslyke@gmail.com>
2018-04-16 02:19:28 -0400

    Set version to 0.9

--- src/main/java/net/trentv/musicalenergy/MusicalEnergy.java
+++ src/main/java/net/trentv/musicalenergy/MusicalEnergy.java
@@ -18,7 +18,7 @@ import net.trentv.musicalenergy.config.MusicalEnergyKeybinds;
 public class MusicalEnergy
 {
 	public static final String MODID = "musicalenergy";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "0.1";
 
 	@Mod.Instance(MODID)
 	public static MusicalEnergy instance;
