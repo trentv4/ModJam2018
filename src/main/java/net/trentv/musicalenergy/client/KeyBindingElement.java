1b5e1346cbd2735c25f17ce3ac7a726a8947c71d
Trent VanSlyke <trentvanslyke@gmail.com>
2018-05-03 00:01:23 -0400

    Set all element keybinds to never conflict.

--- src/main/java/net/trentv/musicalenergy/client/KeyBindingElement.java
+++ src/main/java/net/trentv/musicalenergy/client/KeyBindingElement.java
@@ -1,18 +1,35 @@
 package net.trentv.musicalenergy.client;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.settings.KeyBinding;
-import net.minecraftforge.client.settings.KeyConflictContext;
+import net.minecraftforge.client.settings.IKeyConflictContext;
 import net.trentv.musicalenergy.MusicalEnergy;
 import net.trentv.musicalenergy.common.element.Element;
 
 public class KeyBindingElement extends KeyBinding
 {
+	private static final KeyConflictContextNone CONFLICT_NONE = new KeyConflictContextNone();
 	public final Element element;
 
 	public KeyBindingElement(String description, int keyCode, Element element)
 	{
 		super(description, keyCode, MusicalEnergy.MODID);
-		this.setKeyConflictContext(KeyConflictContext.GUI);
+		this.setKeyConflictContext(CONFLICT_NONE);
 		this.element = element;
+	}
+
+	private static class KeyConflictContextNone implements IKeyConflictContext
+	{
+		@Override
+		public boolean isActive()
+		{
+			return Minecraft.getMinecraft().currentScreen != null;
+		}
+
+		@Override
+		public boolean conflicts(IKeyConflictContext other)
+		{
+			return false;
+		}
 	}
 }
